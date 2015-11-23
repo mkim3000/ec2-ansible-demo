633631be2d89bf9272f88765eecb429afb76e15a
mkim3000 <mkim3000@users.noreply.github.com>
2015-11-23 03:12:23 -0800

    Delete admin-openrc.sh

--- misc/admin-openrc.sh
+++
@@ -1,6 +0,0 @@
-export AWS_ACCESS_KEY_ID='AAAAAAAAAAAAAAAAAA'
-export AWS_SECRET_ACCESS_KEY='BBBBBBBBBBBBBBBBBBBBBBBBBBBBB'
-export AWS_REGION='us-west-2'
-export ANSIBLE_HOSTS=/etc/ansible/ec2.py
-export EC2_INI_PATH=/etc/ansible/ec2.ini 
-
